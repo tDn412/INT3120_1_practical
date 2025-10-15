92455fe72ea3540835908a04d3b25c47044315fd
Tu <nhudinhtu1@gmail.com>
2025-10-15 20:02:56 +0700

    rename

---
+++ Unit6_Pathway3_FlightSearch/app/src/main/java/com/example/unit6_pathway3_flightsearch/FlightSearchApplication.kt
@@ -0,0 +1,8 @@
+package com.example.unit6_pathway3_flightsearch
+
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FlightSearchApplication : Application()
