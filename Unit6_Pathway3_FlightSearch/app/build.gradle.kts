3e1b8b78d42429af1c772e4bbc1bb5a357b8979e
Tu <nhudinhtu1@gmail.com>
2025-10-15 20:05:11 +0700

    last upload, flightsearch
    fix build error

--- Unit6_Pathway3_FlightSearch/app/build.gradle.kts
+++ Unit6_Pathway3_FlightSearch/app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flightsearch"
+    namespace = "com.example.unit6_pathway3_flightsearch" // SỬA Ở ĐÂY
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.flightsearch"
+        applicationId = "com.example.unit6_pathway3_flightsearch" // VÀ SỬA Ở ĐÂY
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -55,20 +55,28 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    // Room
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
     kapt("androidx.room:room-compiler:2.6.0")
+
+    // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-compiler:2.48")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
